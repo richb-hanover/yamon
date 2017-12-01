c856c6d12a0d2b4086fe933f466022cd89c0404e
Rich Brown <richb.hanover@gmail.com>
2017-12-01 14:27:56 -0500

    Track the images that web pages request

---
+++ from_main_site/images/retr.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+curl -O http://usage-monitoring.com/current/images/paypal.png
+curl -O http://usage-monitoring.com/current/images/icons.png
+curl -O http://usage-monitoring.com/current/images/export.png
+curl -O http://usage-monitoring.com/current/images/edit.png
+curl -O http://usage-monitoring.com/current/images/sort-a.png
+curl -O http://usage-monitoring.com/current/images/edit-cursor.png
+curl -O http://usage-monitoring.com/current/images/grippy.png
+curl -O http://usage-monitoring.com/current/images/flags16.png
+curl -O http://usage-monitoring.com/current/images/yamon-logo.png
+curl -O http://usage-monitoring.com/current/images/waiting.gif
+curl -O http://usage-monitoring.com/current/images/buttons.png
+curl -O http://usage-monitoring.com/current/images/tabs.png
+curl -O http://usage-monitoring.com/current/images/sad.gif
+
